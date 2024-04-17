552b8bde0ede8b545b009f1e50aae401755d402a
Ilya Suvorov <vsilux@gmail.com>
2024-04-17 11:14:36 +0300

    added logo

--- app/src/components/NavBar.svelte
+++ app/src/components/NavBar.svelte
@@ -1,6 +1,8 @@
 <!-- NavigationBar.svelte -->
 
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	interface Button {
 		label: string;
 		action: () => void; // Define action as a function type
@@ -9,28 +11,39 @@
 	// let iconUrl =
 	// 'https://cdn.sanity.io/images/aik9dq8j/production/6ab29e7b39c39fc8d0dc8657f8f1661b85a4d2aa-24x16.svg';
 	export let buttons: Button[] = [];
+
+	let backgroundOpacity = 0;
+
+	function updateOpacity() {
+		const scrollY = window.scrollY;
+		const screenHeight = window.innerHeight;
+		const startChanging = screenHeight / 2 - 200; // 100px before the center of the screen
+		const endChanging = screenHeight / 2 + 200; // 100px after the center of the screen
+
+		if (scrollY < startChanging) {
+			backgroundOpacity = 0;
+		} else if (scrollY > endChanging) {
+			backgroundOpacity = 1;
+		} else {
+			// Linear interpolation between startChanging and endChanging
+			backgroundOpacity = (scrollY - startChanging) / (endChanging - startChanging);
+		}
+
+		window.requestAnimationFrame(updateOpacity);
+	}
+
+	// Safeguard browser-specific code
+	onMount(() => {
+		window.requestAnimationFrame(updateOpacity);
+
+		// Cleanup on component destruction
+		return () => {};
+	});
 </script>
 
-<div class="navbar">
+<div class="navbar" style="background-color: rgba(0, 0, 0, {backgroundOpacity});">
 	<div class="left_content">
-		<svg
-			class="icon"
-			width="24"
-			height="16"
-			viewBox="0 0 24 16"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<path
-				d="M0 14.1222L7.78959 7.84287L0 2.13441L1.29827 0.421875H18.1757L19.474 2.13441H22.0705L23.3688 3.84695L1.29827 15.8347L0 14.1222Z"
-				fill="#FFFFFF"
-			/>
-		</svg>
-		<div class="nav_title">
-			<span style="font-weight: 500; color: black">Aircraft&nbsp;</span><span
-				style="font-weight: 300; color: black">Designs</span
-			>
-		</div>
+		<img src="/logo.svg" alt="Logo" class="logo" />
 	</div>
 	<div class="button-container">
 		{#each buttons as { label, action }}
@@ -45,44 +58,56 @@
 		display: flex;
 		z-index: 10;
 		position: fixed;
-
-		height: 38px;
 		display: flex;
 		align-items: center;
-		font-size: var(--font-size-4);
 		/* position: fixed; */
 		justify-content: space-between; /* Aligns items on opposite ends */
 		top: 0;
 		left: 0;
-		filter: invert(1);
-		mix-blend-mode: difference;
-		background-color: transparent;
-		color: black;
-		padding: 10px 20px;
-		width: 100%;
+		right: 0;
+		transition: background-color 0.3s ease;
+		color: white;
+		padding: 16px 35px 16px 35px;
 		will-change: transform;
 	}
 
-	.link-button {
-		background: none;
-		border: none;
-		color: black;
-		cursor: pointer;
-		padding: 5px;
-		margin-right: 10px;
-		font-weight: 400; /* Inherits font styles from parent, including size and family */
-		font-size: var(--font-size-2);
-		text-align: left; /* Aligns text inside button as if it were a link */
-		text-decoration: none; /* Optional: if you want underline on hover, add it here */
+	.button-container {
+		margin-bottom: 1px;
 	}
 
-	.button-container {
-		margin-right: 2em;
+	.link-button {
+		vertical-align: middle;
+		margin: 0;
+		padding: 0;
+		background: none;
+		border: none;
+		color: white;
+		position: relative;
+		cursor: pointer;
+		display: inline-block;
+		font-family: 'PP Neue Montreal';
+		font-weight: 300;
+		font-size: 1em;
+		line-height: 110%;
+		letter-spacing: -1%;
+		text-decoration: none;
+		text-align: right; /* Aligns text inside button as if it were a link */
+	}
+
+	.link-button::after {
+		content: ''; /* Необхідно для відображення псевдоелемента */
+		position: absolute;
+		left: 0; /* Початок з лівого краю кнопки */
+		right: 0; /* Простягання до правого краю кнопки */
+		bottom: -1px; /* Відступ знизу */
+		height: 1px; /* Висота підкреслення */
+		background-color: white; /* Колір підкреслення */
+		opacity: 1; /* Непрозорість для видимості */
 	}
 
 	.link-button:hover,
 	.link-button:focus {
-		text-decoration: underline; /* Adds underline on hover/focus for better UX */
+		text-decoration: none; /* Adds underline on hover/focus for better UX */
 		outline: none; /* Removes default focus outline, add custom if needed for visibility */
 	}
 
@@ -91,29 +116,7 @@
 		align-items: center;
 	}
 
-	.icon {
-		width: 24px;
-		height: 16px;
-		fill: currentColor;
-		filter: invert(1);
-		mix-blend-mode: difference;
-	}
-
-	.link-button {
-		background: none;
-		border: none;
-		color: black;
-		cursor: pointer;
-		padding: 5px;
-		margin-right: 10px;
-		font: inherit; /* Inherits font styles from parent, including size and family */
-		text-align: left; /* Aligns text inside button as if it were a link */
-		text-decoration: none; /* Optional: if you want underline on hover, add it here */
-	}
-
-	.link-button:hover,
-	.link-button:focus {
-		text-decoration: none; /* Adds underline on hover/focus for better UX */
-		outline: none; /* Removes default focus outline, add custom if needed for visibility */
+	.logo {
+		fill: white;
 	}
 </style>
